f601c7a431612f1fd942971b8b074b4aca215c4f
Tomas Tisocco <tomastisocco97@gmail.com>
2024-11-14 19:30:35 -0300

    Init project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/wedding/invitation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.wedding_invitation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:wedding_invitation/app/app.dart';
+import 'package:wedding_invitation/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
